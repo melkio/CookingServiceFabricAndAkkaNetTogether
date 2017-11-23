6d83700d8ba0bb6a20c97bf7b4ff8da60137f04e
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 22:47:13 +0100

    configure PongActorSystem for remoting

--- src/PongHost/Program.cs
+++ src/PongHost/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace PongHost
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+
+namespace PongHost
 {
     class Program
     {
+        private static readonly string Hocon = @"
+akka {
+    actor {
+        provider = remote
+    }
+    remote {
+        dot-netty.tcp {
+            port = 9002 
+            hostname = localhost
+        }
+    }
+}";
         static void Main(string[] args)
         {
+            var config = ConfigurationFactory.ParseString(Hocon);
+            var system = ActorSystem.Create("Pong", config);
+
+            Console.ReadLine();
         }
     }
 }
