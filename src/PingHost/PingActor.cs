933c24c65e079e54f6ad41d0ddcc2c24bbe466ad
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 23:00:35 +0100

    add handle Start message to PingActor

--- src/PingHost/PingActor.cs
+++ src/PingHost/PingActor.cs
@@ -8,7 +8,15 @@ namespace PingHost
     {
         public PingActor()
         {
+            Receive<Start>(message => Handle(message));
             Receive<Pong>(message => Handle(message));
+        }
+
+        private void Handle(Start message)
+        {
+            Context
+                .ActorSelection("akka.tcp://Pong@localhost:9002/user/pong")
+                .Tell(new Ping(1));
         }
 
         private void Handle(Pong message)
