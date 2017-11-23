8885e710e95c25cbc7f5461754cd3ec8cb5eb845
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 16:46:23 +0100

    add GetValue (request/response) message(s)

---
+++ src/AkkaIntro/GetValue.cs
@@ -0,0 +1,25 @@
+﻿namespace AkkaIntro
+{
+    public class GetValue
+    {
+        public class Request
+        {
+            public string CounterId { get; }
+
+            public Request(string counterId)
+            {
+                CounterId = counterId;
+            }
+        }
+
+        public class Response
+        {
+            public int Value { get; }
+
+            public Response(int value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
