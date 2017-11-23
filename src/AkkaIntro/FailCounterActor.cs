ce315e6de99a9aa592a2b16056d3a81e29c19418
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 16:21:30 +0100

    handle Count message into FailCounterActor (with failure)

--- src/AkkaIntro/FailCounterActor.cs
+++ src/AkkaIntro/FailCounterActor.cs
@@ -1,8 +1,31 @@
 ﻿using Akka.Actor;
+using System;
 
 namespace AkkaIntro
 {
     public class FailCounterActor : ReceiveActor
     {
+        private const int MAX = 5;
+
+        private int value;
+
+        public FailCounterActor()
+        {
+            Receive<Count>(message => Handle(message));
+        }
+
+        private void Handle(Count message)
+        {
+            value++;
+            if (value >= MAX)
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            PrintCurrentValue();
+        }
+
+        private void PrintCurrentValue()
+        {
+            Console.WriteLine($"Counter #{Self.Path.Name}: {value}");
+        }
     }
 }
