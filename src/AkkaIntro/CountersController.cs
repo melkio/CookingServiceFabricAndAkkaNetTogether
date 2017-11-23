a43ff524563919e1fa57c8839acf854d94ff8694
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 16:53:14 +0100

    add CountersController.Get(counterId) api

---
+++ src/AkkaIntro/CountersController.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AkkaIntro
+{
+    [RoutePrefix("api/counters")]
+    public class CountersController : ApiController
+    {
+        [Route("{counterId}")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(string counterId)
+        {
+            var request = new GetValue.Request(counterId);
+            var response = await ActorsEnvironment.Gateway.Ask<GetValue.Response>(request);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { value = response.Value });
+        }
+    }
+}
