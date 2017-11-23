28ab3e8b9a84d28613fac1b8d51e5019bc05e4f1
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 16:06:28 +0100

    add Tick handler to TickActor

--- src/AkkaIntro/TickActor.cs
+++ src/AkkaIntro/TickActor.cs
@@ -1,8 +1,23 @@
 ﻿using Akka.Actor;
+using System;
 
 namespace AkkaIntro
 {
     public class TickActor : ReceiveActor
     {
+        private Random random = new Random(DateTime.Now.Millisecond);
+
+        public TickActor()
+        {
+            Receive<Tick>(message => Handle(message));
+        }
+
+        private void Handle(Tick message)
+        {
+            var counterId = random.Next(1, 5);
+
+            Context.ActorSelection("/user/counter")
+                .Tell(new Count($"{counterId}"));
+        }
     }
 }
