cb8689d0007288fbc64577f617dc4accbceb0ca0
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 22:57:29 +0100

    add handler for Ping message to PongActor

--- src/PongHost/PongActor.cs
+++ src/PongHost/PongActor.cs
@@ -1,8 +1,20 @@
-﻿using Akka.Actor;
+﻿using System;
+using Akka.Actor;
+using Common.Messages;
 
 namespace PongHost
 {
     public class PongActor : ReceiveActor
     {
+        public PongActor()
+        {
+            Receive<Ping>(message => Handle(message));
+        }
+
+        private void Handle(Ping message)
+        {
+            Console.WriteLine($"Ping received with value {message.Value}");
+            Sender.Tell(new Pong(message.Value + 1));
+        }
     }
 }
