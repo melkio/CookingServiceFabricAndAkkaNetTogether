70110df8d2a13806eb48fccf69e68da8289594cc
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 14:54:57 +0100

    add behaviors to CounterActor

--- src/AkkaIntro/CounterActor.cs
+++ src/AkkaIntro/CounterActor.cs
@@ -5,17 +5,35 @@ namespace AkkaIntro
 {
     public class CounterActor : ReceiveActor
     {
+        private const int MIN = -5;
+        private const int MAX = 5;
+
         private int value;
 
-        public CounterActor()
+        public CounterActor() => Become(Up);
+
+        private void Up()
         {
-            Receive<Count>(message => Handle(message));
+            Receive<Count>(message =>
+            {
+                value++;
+                if (value >= MAX)
+                    Become(Down);
+
+                Console.WriteLine($"Current value: {value}");
+            });
         }
 
-        private void Handle(Count message)
+        private void Down()
         {
-            value++;
-            Console.WriteLine($"Current value: {value}");
+            Receive<Count>(message =>
+            {
+                value--;
+                if (value <= MIN)
+                    Become(Up);
+
+                Console.WriteLine($"Current value: {value}");
+            });
         }
     }
 }
