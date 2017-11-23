18f58176020739d821757c2455f1fa0733a15d7b
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 15:24:25 +0100

    handle Count message into CounterGatewayActor

--- src/AkkaIntro/CounterGatewayActor.cs
+++ src/AkkaIntro/CounterGatewayActor.cs
@@ -4,6 +4,18 @@ namespace AkkaIntro
 {
     public class CounterGatewayActor : ReceiveActor
     {
+        public CounterGatewayActor()
+        {
+            Receive<Count>(message => Handle(message));
+        }
 
+        private void Handle(Count message)
+        {
+            var actor = Context.Child(message.CounterId);
+            if (actor.IsNobody())
+                actor = Context.ActorOf<CounterActor>(message.CounterId);
+
+            actor.Forward(message);
+        }
     }
 }
