e87e53b090f96a6e19385f9ee8531f934445883b
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-23 22:45:06 +0100

    configure PingActorSystem for remoting

--- src/PingHost/Program.cs
+++ src/PingHost/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace PingHost
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+
+namespace PingHost
 {
     class Program
     {
+        private static readonly string Hocon = @"
+akka {
+    actor {
+        provider = remote
+    }
+    remote {
+        dot-netty.tcp {
+            port = 9001 
+            hostname = localhost
+        }
+    }
+}";
         static void Main(string[] args)
         {
+            var config = ConfigurationFactory.ParseString(Hocon);
+            var system = ActorSystem.Create("Ping", config);
+
+            Console.ReadLine();
         }
     }
 }
