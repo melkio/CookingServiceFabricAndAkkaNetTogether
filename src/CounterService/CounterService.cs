0c0cb4424ee26e7d4d09a4ef46ca72251d6ec84c
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-24 15:17:13 +0100

    add CounterService project (empty)

---
+++ src/CounterService/CounterService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace CounterService
+{
+    internal sealed class CounterService : StatelessService
+    {
+        public CounterService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            long iterations = 0;
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/CounterService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace CounterService
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                ServiceRuntime.RegisterServiceAsync("CounterServiceType",
+                    context => new CounterService(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(CounterService).Name);
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
