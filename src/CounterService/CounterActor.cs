7bc27c4a587fe24a6dfbe990ab62481d1c49f3ad
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-24 15:45:34 +0100

    add and configure CounterGatewayActor and CounterActor

---
+++ src/CounterService/CounterActor.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using Common.Messages;
+
+namespace CounterService
+{
+    public class CounterActor : ReceiveActor
+    {
+        private const int MIN = -5;
+        private const int MAX = 5;
+
+        private int value;
+
+        public CounterActor() => Become(Up);
+
+        private void Up()
+        {
+            Receive<Count>(message =>
+            {
+                value++;
+                if (value >= MAX)
+                    Become(Down);
+            });
+        }
+
+        private void Down()
+        {
+            Receive<Count>(message =>
+            {
+                value--;
+                if (value <= MIN)
+                    Become(Up);
+            });
+        }
+    }
+}
---
+++ src/CounterService/CounterGatewayActor.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using Common.Messages;
+
+namespace CounterService
+{
+    public class CounterGatewayActor : ReceiveActor
+    {
+        public CounterGatewayActor()
+        {
+            Receive<Count>(message => Handle(message));
+        }
+
+        private void Handle(Count message)
+        {
+            var actor = Context.Child(message.CounterId);
+            if (actor.IsNobody())
+                actor = Context.ActorOf<CounterActor>(message.CounterId);
+
+            actor.Forward(message);
+        }
+    }
+}
