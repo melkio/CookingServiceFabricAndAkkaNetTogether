b23cfed8c9684055d5d75ea5707ee7b28714f919
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-24 15:24:22 +0100

    add component acts as reverse proxy

---
+++ src/TickerService/ServiceProxy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace TickerService
+{
+    public class ServiceProxy
+    {
+        public static string Resolve(string serviceName)
+        {
+            var resolver = ServicePartitionResolver.GetDefault();
+
+            var partition = resolver
+                .ResolveAsync(new Uri(serviceName), new ServicePartitionKey(), CancellationToken.None)
+                .GetAwaiter()
+                .GetResult();
+            var endpoint = partition.GetEndpoint();
+            var addresses = JObject.Parse(endpoint.Address);
+
+            return (string)addresses["Endpoints"].First();
+        }
+    }
+}
