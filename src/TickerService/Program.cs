6298bd46514f193b5de50e5ca6d1eda42e02fdb9
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2017-11-24 12:45:25 +0100

    add TickerService project (empty)

---
+++ src/TickerService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace TickerService
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                ServiceRuntime.RegisterServiceAsync("TickerServiceType",
+                    context => new TickerService(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(TickerService).Name);
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
